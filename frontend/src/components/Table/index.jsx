fb1a23fa0a1079cdcefeab97a3359353958860ce
raqueldesa <silva.raquel.de.sa@gmail.com>
2022-11-12 18:14:00 -0400

    fix: aparecendo dados

--- frontend/src/components/Table/index.jsx
+++ frontend/src/components/Table/index.jsx
@@ -1,49 +1,7 @@
-import { useState } from "react";
-
-const Table = () => {
-  const [dates, setDate] = useState([
-    {
-      id: 1,
-      data: "2022-03-17",
-      open: 54.19,
-      high: 54.02,
-      low: 54.02,
-      close: 54.6,
-      volume: 179.189,
-      status: "Ótimo",
-      createdAt: "2022-11-11T20:05:31.000Z",
-      updatedAt: "2022-11-11T20:06:54.000Z",
-    },
-    {
-      id: 2,
-      data: "2022-03-17",
-      open: 54.19,
-      high: 54.02,
-      low: 54.02,
-      close: 54.6,
-      volume: 159.189,
-      status: "Ótimo",
-      createdAt: "2022-11-11T20:05:58.000Z",
-      updatedAt: "2022-11-11T20:06:54.000Z",
-    },
-    {
-      id: 3,
-      data: "2022-03-17",
-      open: 54.19,
-      high: 54.02,
-      low: 54.02,
-      close: 54.6,
-      volume: 159.189,
-      status: "Ótimo",
-      createdAt: "2022-11-11T20:06:05.000Z",
-      updatedAt: "2022-11-11T20:06:54.000Z",
-    },
-  ]);
-
+const Table = ({ data }) => {
   return (
     <div>
-      oi
-      {dates.map((e) => {
+      {data.map((e) => {
         return (
           <div key={e.id}>
             <span style={{ boder: "1px solid black" }}>{e.data} |</span>
--- frontend/src/pages/View/index.jsx
+++ frontend/src/pages/View/index.jsx
@@ -3,25 +3,28 @@ import NavBar from "../../components/NavBar";
 import { Page } from "../Upload/style";
 import { useEffect } from "react";
 import api from "../../services/api";
+import { useState } from "react";
 
 const ViewPage = () => {
-  function loadTechs() {
+  function loadRelatorios() {
     api
       .get(`/relatorios`)
       .then((res) => {
-        console.log(res);
+        setData(res.data);
+        console.log(res.data);
       })
       .catch((err) => err);
   }
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    loadTechs();
+    loadRelatorios();
   }, []);
 
   return (
     <Page>
       <NavBar></NavBar>
-      <Table></Table>
+      <Table data={data}></Table>
     </Page>
   );
 };
